53d68985bb3bc942450830a786e3db348e73280d
arnaudouttier <arnoutti@gmail.com>
2022-01-31 11:43:01 +0100

    nlle beta

--- main.js
+++ main.js
@@ -1,51 +1,49 @@
 const activeCurrentNavLink = () => {
-  document.querySelectorAll("nav ul li").forEach(navLink => 
-    navLink.addEventListener("click", event =>{
-      let current = document.querySelector(".active");
-      event.currentTarget.classList.add("active");
-      current.classList.remove("active");
-    } )
+  document.querySelectorAll('nav ul li').forEach((navLink) =>
+    navLink.addEventListener('click', (event) => {
+      let current = document.querySelector('.active')
+      event.currentTarget.classList.add('active')
+      current.classList.remove('active')
+    }),
   )
 }
 
-const reduire = (element) => {
-  let posteader = element.querySelector('.post .post_eader').getBoundingClientRect().height
-  element.style.height = posteader + 'px'
-  return posteader
+document
+  .querySelectorAll('.card')
+  .forEach((item) => item.addEventListener('mouseover', () => expandCard(item)))
+
+document
+  .querySelectorAll('.card')
+  .forEach((item) => item.addEventListener('mouseout', () => reduceCard(item)))
+
+const reduceCard = (element) => {
+  let postheader = element
+    .querySelector('.post .post_header')
+    .getBoundingClientRect().height
+  element.style.height = postheader + 'px'
+  return postheader
 }
 
-const etendre = (element) => {
+const expandCard = (element) => {
   let postContent = element.querySelector('.post .post_content')
   postContent.style.padding = '2rem'
- const elementA = element.scrollHeight
- element.style.height = elementA + 'px'
+  const elementA = element.scrollHeight
+  element.style.height = elementA + 'px'
 }
+
+document.querySelectorAll('button.btn-select').forEach((btnSelect) => {
+  btnSelect.addEventListener('click', (event) => {
+    setCssVariable(event.currentTarget.value)
+  })
+})
 
 const setCssVariable = (element) => {
   const rootCss = document.querySelector(':root')
   const rootCssStyle = getComputedStyle(rootCss)
   let rootVarColor = rootCssStyle.getPropertyValue(`--${element}_color`)
-  if(rootVarColor ){
-    rootCss.style.setProperty('--primary_color', rootVarColor);
+  if (rootVarColor) {
+    rootCss.style.setProperty('--primary_color', rootVarColor)
   }
 }
 
-document.querySelector('select').addEventListener('change' ,  event =>{
-  setCssVariable(event.currentTarget.value)
-})
-
-document.querySelectorAll(".card").forEach(item => 
-  item.addEventListener("mouseover", () =>etendre(item)),
-)
-
-document.querySelectorAll(".card").forEach(item => 
-  item.addEventListener("mouseout", () =>reduire(item))
-)
-
 activeCurrentNavLink()
-
-
-
-
-
-
